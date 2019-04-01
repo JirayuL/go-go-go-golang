6e4b4fa6033381e3522607dfb946ff2248cd7550
Jirayu Laungwilawan <jirayu.l@ku.th>
2019-04-01 18:43:10 +0700

    Add example of channel in pipeline

---
+++ pipeline.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func generator() chan int {
+	output := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			output <- i
+		}
+		close(output)
+	}()
+	return output
+}
+
+func multiply(input chan int) chan int {
+	output := make(chan int)
+	go func() {
+		for num := range input {
+			output <- num * num
+		}
+		close(output)
+	}()
+	return output
+}
+
+func print(input chan int) {
+	for num := range input {
+		fmt.Println(num)
+	}
+
+}
+
+func add(input chan int) chan int {
+	output := make(chan int)
+	go func() {
+		for num := range input {
+			output <- num + num
+		}
+		close(output)
+	}()
+	return output
+}
+
+func mainPP() {
+	g := generator()
+	m := multiply(g)
+	a := add(m)
+	print(a)
+}
